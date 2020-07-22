b57bd46a06c73c1eaaffec2dd5efa5488cdb53ef
basil <basilsunnyalukka@gmail.com>
2020-07-22 16:58:14 +0530

    second commit

--- time_calculator.py
+++ time_calculator.py
@@ -1,31 +1,21 @@
 def add_time(start, duration, *day):
-    
-    #####  spliting strings
+
     start_h, start_m, period = start.replace(':',' ').replace(',', ',').split()
     duration_h, duration_m = duration.split(':')
-    
-    #####  calculating starting minutes and duration minutes
     start_in_minutes = int(start_m) + (int(start_h) * 60)
-    # print('start_in_minutes:', start_in_minutes)
-    
     duration_in_minutes = int(duration_m) + (int(duration_h) * 60)
-    # print('duration_in_minutes:', duration_in_minutes)
-    
     total_minutes = start_in_minutes + duration_in_minutes
-    # print('total_minutes:', total_minutes)
-    
-    
+
     def new_hours(minutes):
     
         new_hour = (minutes // 60) % 12
         if new_hour == 0:
             new_hour = 12
-        # print('new_hour:', new_hour)
+        
         return new_hour
     
     new_h = new_hours(total_minutes)
-    
-    
+ 
     def new_minutes(minutes):
         
         new_minute = (minutes % 60)
@@ -33,7 +23,7 @@ def add_time(start, duration, *day):
             new_minute = '0' + str(new_minute)
         else:
             new_minute = str(new_minute)
-        # print('new_minute:', new_minute)
+       
         return new_minute
     
     new_m = new_minutes(total_minutes)
@@ -50,8 +40,7 @@ def add_time(start, duration, *day):
         return new_per
         
     new_p = new_period(period)
-    
-    
+ 
     def count_days(p):
     
         if p == 'PM' and int(start_h) == 12:
@@ -67,8 +56,7 @@ def add_time(start, duration, *day):
         return n_day
     
     n_day = count_days(period)
-    
-    
+ 
     def days_pass(days):            
         
         if days == 1:
@@ -81,19 +69,17 @@ def add_time(start, duration, *day):
     
     d_pass = days_pass(n_day)
 
-    
     def week_day(d):
         
         if d:
             d = str(*day).title()
-            # print('current day:', d)
+          
             week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-            idx = [i for i, n in enumerate(week) if n == d]  # List comprehension
+            idx = [i for i, n in enumerate(week) if n == d]  
             if idx:
                 idx = idx[0]
             else:
                 idx = 0
-            # print('idx:', idx)  # index of the current day
 
             n_day_idx = 0
             if int(n_day) == int(idx):
@@ -105,7 +91,7 @@ def add_time(start, duration, *day):
             else:
                 n_day_idx = int(idx) + int(n_day)
             
-            # print('n_day_idx:', n_day_idx)  # index of the future day
+            
             new_day = week[n_day_idx]
         else:
             new_day = ''
@@ -113,25 +99,23 @@ def add_time(start, duration, *day):
         return new_day
         
     new_d = week_day(day)
-    
-    
+
     def results(new_h, new_m, new_p, d_pass, new_d):
  
         if d_pass and new_d:
-            # print(str(new_h) + ':' + str(new_m) + ' ' + str(new_p) + ', ' + str(new_d) + ' ' + str(d_pass))
+            
             new_time = str(new_h) + ':' + str(new_m) + ' ' + str(new_p) + ', ' + str(new_d) + ' ' + str(d_pass)
         elif d_pass != '' and new_d == '':
-            # print(str(new_h) + ':' + str(new_m) + ' ' + str(new_p) + ' ' + str(d_pass))
+            
             new_time = str(new_h) + ':' + str(new_m) + ' ' + str(new_p) + ' ' + str(d_pass)
         elif new_d != '' and d_pass == '':
-            # print(str(new_h) + ':' + str(new_m) + ' ' + str(new_p) + ', ' + str(new_d))
+          
             new_time = str(new_h) + ':' + str(new_m) + ' ' + str(new_p) + ', ' + str(new_d)
         else:
-            # print(str(new_h) + ':' + str(new_m) + ' ' + str(new_p))
+          
             new_time = str(new_h) + ':' + str(new_m) + ' ' + str(new_p)
         return new_time
-            
-              
+          
     new_time = results(new_h, new_m, new_p, d_pass, new_d)
 
     return new_time
